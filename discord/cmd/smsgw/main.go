11d4b3a7c93b4ec588ce83e2497fce839e71d43f
phaedrusflow <map@qompass.ai>
2025-05-04 10:18:28 -0700

    Update metadata, badge, and citation for 2025-05-04

---
+++ discord/cmd/smsgw/main.go
@@ -0,0 +1,8 @@
+// ~/.GH/Qompass/Go/Azimuth/discord/cmd/smsgw/main.go
+// --------------------------------------------------
+// Copyright (C) 2025 Qompass AI, All rights reserved
+
+package main
+
+// TODO: Implement Twilio/Vonage webhook → NATS bridge
+func main() {}
